8be648888ea2aaed0c63720321fa2ed5fbcbb86d
Sameera Abeywickrama <sameeraabeywickrama@Sameeras-iMac.local>
2018-08-26 22:55:09 +0530

    FEAT: Add rentals module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 // Connect mongoose
 mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true})
@@ -18,7 +19,8 @@ app.use(express.json());
 
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
-app.use('/api/movies', movies)
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
